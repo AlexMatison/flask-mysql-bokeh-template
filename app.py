b101ade41d39baf1b5fed5ed109375bcf54123e3
AlexMatison <AlexMatison@users.noreply.github.com>
2018-11-12 17:11:48 +1030

    Getting dockerfile to work

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask
 import web
+import settings
 
 app = Flask(__name__)
-app.register_blueprint(web.web_blueprint)
+
+def configure_app(flask_app):
+    #flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
+    pass
+
+def initialise_app(flask_app):
+    configure_app(flask_app)
+    flask_app.register_blueprint(web.web_blueprint)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    initialise_app(app)
+    app.run(debug=True,
+            host=settings.FLASK_SERVER_NAME,
+            port=settings.FLASK_SERVER_PORT)
